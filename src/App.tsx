1191fb0b712c1bfe590b8ef4bdf05450617d0c63
Kwuraa <kwuraa@gmail.com>
2024-06-18 12:55:45 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card } from "./components/Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World!!
+      <div>
+        <h1>Card</h1>
+        <p>Matheus Morais Kawamura</p>
+      </div>
+      <Card />
     </div>
   );
 }
 
 export default App;
+
+//! timing aula 6:18
