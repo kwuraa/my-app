483315bba96c6f10a65bc101754b24a2a01e7cf9
Kwuraa <kwuraa@gmail.com>
2024-06-19 01:42:54 -0300

    Conhecendo o ambiente

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import React from "react";
 import { Card } from "./components/Card";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
-    <div>
+    <Layout>
       Hello World!!
-      <div>
-        <h1>Card</h1>
-        <p>Matheus Morais Kawamura</p>
-      </div>
-      <Card />
-    </div>
+      <Card
+        id={1}
+        paragraph="Typescript"
+        details="TS para front-end e Back-end"
+      />
+      <Card id={2} paragraph="Html" details="HTML para Front-end" />
+      <Card id={3} paragraph="Java" details="Java para Back-end" />
+    </Layout>
   );
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,16 @@
-export const Card = () => {
-  return <div>Componente Card</div>;
+interface ICard {
+  id: number;
+  paragraph: string;
+  details: string;
+}
+
+export const Card = ({ id, paragraph, details }: ICard) => {
+  console.log(id);
+  return (
+    <div>
+      <h1>Card {id}</h1>
+      <p>{paragraph}</p>
+      <p>{details}</p>
+    </div>
+  );
 };
