5d74e12c884fb826865c53fd3e4711db139cb186
kwuraa <kwuraa@gmail.com>
2024-07-31 10:13:44 -0300

    "Adicionada nova função changeLocalStorage e atualizados testes de localStorage"

--- src/services/storage.test.tsx
+++ src/services/storage.test.tsx
@@ -1,9 +1,14 @@
-import { createLocalStorage, getAllLocalStorage } from "./storage";
+import {
+  changeLocalStorage,
+  createLocalStorage,
+  getAllLocalStorage,
+} from "./storage";
 
 const dioBank = {
   login: false,
 };
 describe("storage", () => {
+  const mockSetItem = jest.spyOn(Storage.prototype, "setItem");
   it("deve retornar o objeto no localstorage", () => {
     const mockGetItem = jest.spyOn(Storage.prototype, "getItem");
     getAllLocalStorage();
@@ -11,8 +16,15 @@ describe("storage", () => {
   });
 
   it("deve criar o objeto no local storage", () => {
-    const mockSetItem = jest.spyOn(Storage.prototype, "setItem");
     createLocalStorage();
+    expect(mockSetItem).toHaveBeenCalledWith(
+      "diobank",
+      JSON.stringify(dioBank)
+    );
+  });
+
+  it("deve alterar o valor do objeto no local Storage", () => {
+    changeLocalStorage(dioBank);
     expect(mockSetItem).toHaveBeenCalledWith(
       "diobank",
       JSON.stringify(dioBank)
--- src/services/storage.tsx
+++ src/services/storage.tsx
@@ -1,11 +1,19 @@
+interface IDioBank {
+  login: Boolean;
+}
+
 const dioBank = {
   login: false,
 };
 
-export const getAllLocalStorage = () => {
+export const getAllLocalStorage = (): string | null => {
   return localStorage.getItem("diobank");
 };
 
 export const createLocalStorage = (): void => {
   localStorage.setItem("diobank", JSON.stringify(dioBank));
+};
+
+export const changeLocalStorage = (diobank: IDioBank) => {
+  localStorage.setItem("diobank", JSON.stringify(diobank));
 };
